b38e8c7a3237e8b3c7f6c59d25156bd53532d633
Karijanna Miller <narae7595@ksu.edu>
2019-10-20 23:34:55 -0500

    finnish

--- MenuTest/OrderTest.cs
+++ MenuTest/OrderTest.cs
@@ -19,5 +19,32 @@ namespace MenuTest
             order.Items.Add(new Sodasaurus());
             Assert.Equal(8.02, order.TotalCost);
         }
+        [Fact]
+        public void OrderTestMultipleTimes()
+        {
+            Order order = new Order();
+            DinoNuggets dn = new DinoNuggets();
+            dn.AddNugget();
+            JurassicJava coffee = new JurassicJava();
+            coffee.Size = Size.Medium;
+            MeteorMacAndCheese mmc = new MeteorMacAndCheese();
+            mmc.Size = Size.Large;
+            order.Items.Add(dn);
+            order.Items.Add(coffee);
+            order.Items.Add(mmc);
+            order.Items.Add(new VelociWrap());
+            Assert.Equal(14.30, order.TotalCost);
+        }
+        [Fact]
+        public void OrderTotalShouldNotBeNegative()
+        {
+            Order order = new Order();
+            Fryceritops ft = new Fryceritops();
+            ft.Price = -45;
+            order.Items.Add(new PrehistoricPBJ());
+            order.Items.Add(new Tyrannotea());
+            order.Items.Add(ft);
+            Assert.Equal(0, order.TotalCost);
+        }
     }
 }
